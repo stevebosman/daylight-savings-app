4a263b1e0e2d6625f47b317dcedd51c402b73269
Steve Bosman <stevebosman@gmail.com>
2020-03-31 00:48:22 +0100

    Add basic preferences

--- app/src/main/java/uk/co/stevebosman/daylight/SettingsActivity.kt
+++ app/src/main/java/uk/co/stevebosman/daylight/SettingsActivity.kt
@@ -3,6 +3,8 @@ package uk.co.stevebosman.daylight
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 class SettingsActivity : AppCompatActivity() {
@@ -21,7 +23,20 @@ class SettingsActivity : AppCompatActivity() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
         }
-    }
+        private val DIALOG_FRAGMENT_TAG = "CustomPreference"
+
+        override fun onDisplayPreferenceDialog(preference: Preference) {
+            if (parentFragmentManager.findFragmentByTag(DIALOG_FRAGMENT_TAG) != null) {
+                return
+            }
+            if (preference is TimePreference) {
+                val f: DialogFragment = TimeDialog(preference.getKey())
+                f.setTargetFragment(this, 0)
+                f.show(parentFragmentManager, DIALOG_FRAGMENT_TAG)
+            } else {
+                super.onDisplayPreferenceDialog(preference)
+            }
+        }    }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.getItemId()) {
@@ -32,4 +47,5 @@ class SettingsActivity : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
+
 }
